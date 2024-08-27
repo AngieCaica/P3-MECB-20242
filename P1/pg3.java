6b48da881ab61775e8ffec2b8eef58f3fcd69068
AngieCaica <est.angie.caica@unimilitar.edu.co>
2024-08-27 15:43:41 -0500

    Create pg3.java

---
+++ P1/pg3.java
@@ -0,0 +1,38 @@
+//1.Crear una clase llamada triangulo con atributos base y altura, aplicar encapsulamiento y los metodos apropiados para inicializar o crear objetos con sus respectivos//
+//2. a partir de la clase del punto anterior, crear sobrecarga al constructor y crear dos objetos para cada una de las formas del constructor
+//3. crear dos metodos puros que permitan obtener area y perimetro del triangulo asumiendo que todos son triangulo rectangulo
+Public class Triangulo {
+    
+    private int base;
+    private int altura;
+    
+    public triangulo (){
+     this.base = 8;
+     this.altura = 10; 
+     this. angulo = 5 ;
+     this.suma=1;
+    }
+    
+    public int getbase(){
+        int base = base; 
+    }
+    public void setbase(int base){
+        
+    }
+    public int Setaltura(){
+        int altura = altura;
+    }
+    
+    public void area( int base, int altura){
+        area= b * a/2;
+        return area;
+    }
+    public void angulo (int base, int altura, int angulo){
+        
+    }
+    public int perimetro(int base, int altura){
+        perimetro = math.pqrs(base) * match.pqrs(altura+altura);
+        return perimetro;
+    }
+
+}
