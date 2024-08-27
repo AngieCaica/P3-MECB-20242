fa809704666f05e73c9623bcd940322caab64d23
AngieCaica <est.angie.caica@unimilitar.edu.co>
2024-08-27 15:32:43 -0500

    Create pg2.java

---
+++ P1/pg2.java
@@ -0,0 +1,33 @@
+//Crear una clase llamada triangulo con atributos base y altura, aplicar encapsulamiento y los metodos apropiados para inicializar o crear objetos con sus respectivos//
+// a partir de la clase del punto anterior, crear sobrecarga al constructor y crear dos objetos para cada una de las formas del constructor
+Public class Triangulo {
+    
+    private int base;
+    private int altura;
+    
+    public triangulo (){
+     this.base = 0;
+     this.altura = 1; 
+     this. angulo = 0;
+     this.suma=1;
+    }
+    
+    public int getbase(){
+        int base = base; 
+    }
+    public void setbase(int base){
+        
+    }
+    public int Setaltura(){
+        int altura = altura;
+    }
+    
+    public void area( int base, int altura){
+        area= b * a/2;
+        
+    }
+    public void angulo (int base, int altura, int angulo){
+        cos= 
+    }
+
+}
