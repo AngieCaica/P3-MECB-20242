e4883382b50a39e3cfdbfd64638dadf9ac45eee4
AngieCaica <est.angie.caica@unimilitar.edu.co>
2024-08-27 15:23:06 -0500

    Create pg1.java

---
+++ P1/pg1.java
@@ -0,0 +1,20 @@
+//Crear una clase llamada triangulo con atributos base y altura, aplicar encapsulamiento y los metodos apropiados para inicializar o crear objetos con sus respectivos//
+
+Public class Triangulo {
+    
+    private int base;
+    private int altura;
+    
+    public triangulo (){
+     this.base = 0;
+     this.altura = 1; 
+    }
+    
+    public int getbase(){
+        int base = base; 
+    }
+    public void setbase(){
+        
+    }
+    public int Setaltura
+}
